010533a37590d023f020ecef9d9b3175f942d83c
Taylor Spear <taylorspear1993@gmail.com>
2025-02-08 16:26:15 -0500

    lets see this

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,14 +1,11 @@
 import { Pool } from "pg";
-import * as dotenv from "dotenv";
-
-dotenv.config({ path: ".env.production" });
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  user: "postgres", // Replace with your actual username
+  host: "blk-411-db.cr8q0mco2kl3.us-east-1.rds.amazonaws.com", // Replace with your actual RDS endpoint
+  database: "blk-411", // Replace with your actual database name
+  password: "B4dbpw4lp34!", // Replace with your actual password
+  port: 5432, // Default PostgreSQL port
 });
 
 export default pool;
